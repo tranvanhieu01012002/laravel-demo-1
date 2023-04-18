28265bc6990d60f51470315d0a8b2bd7e41e9152
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-18 09:27:58 +0700

    fix delay socket

--- app/Events/RoomEvent.php
+++ app/Events/RoomEvent.php
@@ -6,12 +6,12 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Redis;
 
-class RoomEvent implements ShouldBroadcast
+class RoomEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
