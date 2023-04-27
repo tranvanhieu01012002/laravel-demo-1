7b9a030b84bd50ad94e99baa383a7a3d5bd6ba74
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-27 22:54:45 +0700

    remove hook before install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATH="/var/www/back-end.g-learning.click"
-cd $PATH && chmod -R 755 storage bootstrap/cache
+sudo chmod -R 755 $PATH/storage $PATH/bootstrap/cache
 cd $PATH && composer install -n
