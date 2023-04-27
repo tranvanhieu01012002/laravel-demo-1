71ffcdb895c585b0c4b3c50e577cfadb5266f474
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-27 22:27:04 +0700

    add scripts

---
+++ scripts/artisans.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARTISAN="/var/www/back-end.g-learning.click/artisan"
+
+cd /var/www/back-end.g-learning.click && composer dump-autoload
+
+php $ARTISAN route:clear
+php $ARTISAN route:cache
+
+php $ARTISAN cache:clear
+
+php $ARTISAN config:clear
+php $ARTISAN config:cache
+
+chmod -R 755 storage bootstrap/cache
+
+
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /var/www/back-end.g-learning.click && composer install -n
