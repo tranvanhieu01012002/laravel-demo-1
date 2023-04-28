343598473019136705b77474a91df2d7a6679f7f
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-28 10:28:06 +0700

    update 77 for permission

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 composer install --working-dir=/var/www/back-end.g-learning.click
-chmod -R 755 /var/www/back-end.g-learning.click/storage
-chmod -R 755 /var/www/back-end.g-learning.click/cache
+chmod -R 777 /var/www/back-end.g-learning.click/storage
+chmod -R 777 /var/www/back-end.g-learning.click/bootstrap/cache
