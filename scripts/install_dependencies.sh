e7df3475b09e4b744eab8e2b44da1f839fd9592c
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-28 09:29:09 +0700

    update no plugin no script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo chmod -R 755 /var/www/back-end.g-learning.click/storage
 sudo chmod -R 755 /var/www/back-end.g-learning.click/bootstrap/cache
-cd /var/www/back-end.g-learning.click/ && composer install --no-dev -n --prefer-dist
+cd /var/www/back-end.g-learning.click/ && composer install --no-dev -n --prefer-dist --no-plugins --no-scripts
