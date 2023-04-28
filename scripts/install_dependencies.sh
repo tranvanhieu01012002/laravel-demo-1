ced10e607381c4702acd3db1088c47739ddad84b
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-28 10:07:45 +0700

    update order in install dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
+composer install --working-dir=/var/www/back-end.g-learning.click/
 sudo chmod -R 755 /var/www/back-end.g-learning.click/storage
 sudo chmod -R 755 /var/www/back-end.g-learning.click/bootstrap/cache
-composer install --working-dir=/var/www/back-end.g-learning.click/
