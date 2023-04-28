849a352c075837d982d3bd567832de606f92e1f7
010102tranvanhieu <hieu.tran23@student.passerellesnumeriques.org>
2023-04-28 10:04:35 +0700

    update working dir composer

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo chmod -R 755 /var/www/back-end.g-learning.click/storage
 sudo chmod -R 755 /var/www/back-end.g-learning.click/bootstrap/cache
-cd /var/www/back-end.g-learning.click/ && composer install
+composer install --working-dir=/var/www/back-end.g-learning.click/
